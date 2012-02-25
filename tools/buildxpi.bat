77b1bc2903582890b15656f5118034b13a45fe85
Yuan Xulei <vyxl@163.com>
2012-02-25 23:55:00 +0800

    修复打包脚本问题

--- tools/buildxpi.bat
+++ tools/buildxpi.bat
@@ -4,6 +4,7 @@ del /f/q %XPI_NAME%
 cd extension
 ..\tools\7za a ..\%XPI_NAME% chrome\
 ..\tools\7za a ..\%XPI_NAME% defaults\
+..\tools\7za a ..\%XPI_NAME% modules\
 ..\tools\7za a ..\%XPI_NAME% plugins\npfireie.dll
 ..\tools\7za a ..\%XPI_NAME% chrome.manifest
 ..\tools\7za a ..\%XPI_NAME% install.rdf
