8d0ed28e12ae7123d544491d043e40e15235a33a
Yuan Xulei <vyxl@163.com>
2012-02-25 23:10:29 +0800

    添加License申明

--- plugin/IEHostWindow.cpp
+++ plugin/IEHostWindow.cpp
@@ -1,3 +1,9 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * This Source Code is subject to the terms of the Mozilla Public License
+ * version 2.0 (the "License"). You can obtain a copy of the License at
+ * http://mozilla.org/MPL/2.0/.
+ * ***** END LICENSE BLOCK ***** */
+
 // IEHostWindow.cpp : implementation file
 //
 
