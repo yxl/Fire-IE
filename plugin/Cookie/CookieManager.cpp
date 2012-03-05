84744135f763435b56367975da73679b0764cb88
wdeng <wdeng@mozilla.com>
2012-03-05 19:29:19 +0800

    add function for reading and setting IE cookies registry
    Signed-off-by: wdeng <wdeng@mozilla.com>

--- plugin/Cookie/CookieManager.cpp
+++ plugin/Cookie/CookieManager.cpp
@@ -4,4 +4,22 @@
 namespace Cookie
 {
 	CookieManager CookieManager::s_instance;
+	CString CookieManager::ReadIECtrlCookieReg()
+	{
+		TCHAR value[4096];
+		DWORD dwSzie= sizeof(value);
+		long ret = RegGetValue(HKEY_CURRENT_USER, TEXT("Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\User Shell Folders"),TEXT("Cookies"),
+		RRF_RT_ANY|RRF_NOEXPAND,NULL,value,&dwSzie);
+		return value;
+	}
+	void CookieManager::SetIECtrlCookieReg(CString& csCookie)
+	{
+		HKEY key;
+		if(ERROR_SUCCESS == RegOpenKeyEx(HKEY_CURRENT_USER, TEXT("Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\User Shell Folders"), 0, KEY_SET_VALUE, &key))
+		{
+			RegSetValueEx(key, TEXT("Cookies"), 0, REG_SZ, (LPBYTE)csCookie.GetBuffer(), 1024);
+			csCookie.ReleaseBuffer();
+			RegCloseKey(key);
+		}
+	}
 }
--- plugin/Cookie/CookieManager.h
+++ plugin/Cookie/CookieManager.h
@@ -1,12 +1,18 @@
 #pragma once
+#include <cstring>
+
 namespace Cookie
 {
 	class CookieManager
 	{
 	public:
 		static CookieManager s_instance;
+		static CString ReadIECtrlCookieReg();
+		static void SetIECtrlCookieReg(CString& csCookie);
+		//static void RestoreIECookieReg(CString& csIECookie);
 	private:
 		CookieManager(void){}
 		~CookieManager(void){}
+
 	};
 }
