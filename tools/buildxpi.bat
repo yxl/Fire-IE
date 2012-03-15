3390abb3be721e39d0d07225a2610a37853489b3
Yuan Xulei <xyuan@mozilla.com>
2012-03-15 18:29:00 +0800

    Fix bug of buildxpi.xpi that missing packaging the folder "extension/component".

--- tools/buildxpi.bat
+++ tools/buildxpi.bat
@@ -4,6 +4,7 @@ del /f/q %XPI_NAME%
 cd extension
 ..\tools\7za a ..\%XPI_NAME% chrome\
 ..\tools\7za a ..\%XPI_NAME% defaults\
+..\tools\7za a ..\%XPI_NAME% components\
 ..\tools\7za a ..\%XPI_NAME% modules\
 ..\tools\7za a ..\%XPI_NAME% plugins\*.dll
 ..\tools\7za a ..\%XPI_NAME% chrome.manifest
