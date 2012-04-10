3435cee14874170dd48c310dd817bbbc83154797
Yuan Xulei <vyxl@163.com>
2012-04-11 04:01:36 +0800

    remvoe warnings.

--- plugin/IEHostWindow.cpp
+++ plugin/IEHostWindow.cpp
@@ -1611,7 +1611,7 @@ void CIEHostWindow::FBHighlightAll()
 	if (m_strFBText.GetLength() == 0) return;
 
 	long lOriginalIndex = m_lFBCurrentDoc;
-	for (m_lFBCurrentDoc = 0; m_lFBCurrentDoc < static_cast<long>(m_vFBDocs.size()); m_lFBCurrentDoc++)
+	for (m_lFBCurrentDoc = 0; m_lFBCurrentDoc < m_vFBDocs.size(); m_lFBCurrentDoc++)
 	{
 		FBDocFindStatus& dfs = FBGetCurrentDocStatus();
 		long tmp;
