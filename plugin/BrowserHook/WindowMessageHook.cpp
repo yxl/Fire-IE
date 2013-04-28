333b3a94cef19e3e35c882127e49684d2e27249e
Yifan Wu <patwonder@163.com>
2013-04-28 20:36:04 +0800

    Adjust code structure of FilterFirefoxKey.

--- plugin/BrowserHook/WindowMessageHook.cpp
+++ plugin/BrowserHook/WindowMessageHook.cpp
@@ -287,11 +287,13 @@ Exit:
 			switch (keyCode)
 			{
 			case VK_CONTROL: // Only Ctrl is pressed
+				return FALSE;
 			case VK_SHIFT: // Issue #90: Ctrl-Shift switching IME, should not lose focus
 			case VK_SPACE:
 			case VK_PROCESSKEY:
-				TRACE(_T("[WindowMessageHook] VK_SHIFT or VK_PROCESSKEY\n"));
-			case VK_RETURN: // Ctrl-Return, fast post on Baidu Tieba & potentially other places
+				TRACE(_T("[WindowMessageHook] VK_SHIFT, VK_SPACE or VK_PROCESSKEY\n"));
+				return FALSE;
+
 			// The following shortcut keys will be handle by IE control only and won't be sent to Firefox
 			case 'P': // Ctrl+P, Print
 			case 'C': // Ctrl+C, Copy
@@ -306,6 +308,7 @@ Exit:
 			case VK_RIGHT:
 			case VK_UP: // Ctrl+U/D, identical to Up/Down
 			case VK_DOWN:
+			case VK_RETURN: // Ctrl-Return, fast post on Baidu Tieba & potentially other places
 				return FALSE;
 			default:
 				TRACE(_T("[WindowMessageHook] Forwarded firefox key with keyCode = %d\n"), keyCode);
