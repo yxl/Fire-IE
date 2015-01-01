d2350c78a4e4824a1bd3295d825419e4d0b75ad1
Yifan Wu <patwonder@163.com>
2015-01-01 18:17:33 +0800

    Add build scripts for unified xpi.

---
+++ tools/compile-and-build-unified.bat
@@ -0,0 +1,11 @@
+@rem Add path to MSBuild Binaries
+@if exist "%ProgramFiles%\MSBuild\12.0\bin" set PATH=%ProgramFiles%\MSBuild\12.0\bin;%PATH%
+@if exist "%ProgramFiles(x86)%\MSBuild\12.0\bin" set PATH=%ProgramFiles(x86)%\MSBuild\12.0\bin;%PATH%
+
+@rem Call msbuild
+@cd ..
+@MSBuild fireie.msbuild /t:unified
+@if errorlevel 1 (
+  @echo Build failed. See the above output for details.
+  @pause
+)
