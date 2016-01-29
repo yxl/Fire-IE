f46b173b17b742313e1dd5b45eceed163daa8080
Yifan Wu <patwonder@163.com>
2016-01-29 20:17:58 +0800

    Fix compatibility with Firefox 44

--- extension/chrome/content/container.js
+++ extension/chrome/content/container.js
@@ -14,7 +14,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with Fire-IE.  If not, see <http://www.gnu.org/licenses/>.
 */
-let FireIEContainer = {};
+let FireIEContainer = window.FireIEContainer = {};
 
 (function()
 {
